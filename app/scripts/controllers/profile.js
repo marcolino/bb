8b67150de57e681347ee0cce5208e6944a97dfb4
marcolino <marcosolari@gmail.com>
2014-02-28 15:52:07 +0100

    Tutorial completed.

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,23 +1,3 @@
-/*
-'use strict';
- 
-app.controller('ProfileCtrl',
-  function ($scope, $routeParams, Post, User) {
-    $scope.user = User.findByUsername($routeParams.username);
-
-    function populatePosts () {
-      $scope.posts = {};
- 
-      angular.forEach($scope.user.posts, function(postId) {
-        $scope.posts[postId] = Post.find(postId);
-        console.info($scope.posts[postId]);
-      });
-    }
-  
-    $scope.user.$on('loaded', populatePosts);
-
-  });
-*/
 'use strict';
 
 app.controller('ProfileCtrl',
