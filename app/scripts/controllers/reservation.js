b28e7d0968e3c3474d7908ca6fdf7742b666a084
marcolino <marcosolari@gmail.com>
2014-03-12 18:13:16 +0100

    reservation

--- app/scripts/controllers/reservation.js
+++ app/scripts/controllers/reservation.js
@@ -3,13 +3,79 @@
 // handle form submission when the form is completely valid  
 app.controller('ReservationCtrl',
   function ($scope) {
-    $scope.sendForm = function() {
-      window.alert('form valid, sending request...');
+
+    $scope.legend = 'Reservation';
+    $scope.fields = {
+      namesurname: {
+        title: 'Your name and surname',
+        required: true
+      },
+      howmany: {
+        title: 'How many persons you are',
+        required: true
+      },
+      car: {
+        title: 'Do you need parking?',
+        required: true
+      },
+      when: {
+        title: 'The date of your arrival',
+        required: true
+      },
+      time: {
+        title: 'The time of your arrival',
+        required: true
+      },
+      email: {
+        title: 'Your email address',
+        required: true
+      },
+      phone: {
+        title: 'Your phone number',
+        required: false
+      },
+      info: {
+        title: 'Other info...',
+        required: false
+      }
     };
+
+/**/
+    $scope.fields.namesurname.required = true;
+    $scope.namesurnameMinLen = 3;
+    $scope.namesurnameMaxLen = 48;
+    $scope.limit = {};
+    $scope.limit.howmany = {};
+    $scope.limit.howmany.min = 1;
+    $scope.limit.howmany.max = 2;
+    $scope.title = {};
+    $scope.title.namesurname = 'Your name and surname';
+    $scope.title.howmany = 'How many persons you are';
+    $scope.title.car = 'Do you need parking?';
+    $scope.title.when = 'The date of your arrival';
+    $scope.title.time = 'The time of your arrival';
+    $scope.title.email = 'Your email address';
+    $scope.title.phone = 'Your phone number';
+    $scope.title.info = 'Other info...';
+    $scope.required = {};
+    $scope.required.namesurname = true;
+    $scope.required.info = false;
+/**/
+
+    $scope.submitForm = function() {
+      if ($scope.reservation_form.$valid) {
+        // Submit as normal
+        window.alert('reservation form is valid, sending request...');
+      } else {
+        $scope.reservation_form.submitted = true;
+      }
+    };
+/*
     $scope.resetForm = function() {
       $scope.reservation_form.$setPristine(true);
+      $scope.reservation_form.submitted = false;
     };
-    
+*/
 /*
     $scope.$watch('date', function (date) {
       $scope.dateString = dateFilter(date, 'yyyy-MM-dd');
@@ -35,6 +101,7 @@ app.controller('DatepickerCtrl',
     $scope.disabled = function(date, mode) {
       return ( mode === 'day' && ( date.getDate() === 17 ) );
     };
+
 /*
     $scope.todayHighlight = true;
     $scope.dateOptions = {
@@ -65,7 +132,7 @@ app.controller('DatepickerCtrl',
     $scope.toggleWeeks = function () {
       $scope.showWeeks = ! $scope.showWeeks;
     };
-  
+ 
     $scope.clear = function () {
       $scope.dt = null;
     };
@@ -96,5 +163,22 @@ app.controller('DatepickerCtrl',
     $scope.format = $scope.formats[0];
 ./
 */
+  }
+);
+
+app.controller('PhoneCtrl',
+  function ($scope) {
+    $scope.phoneNumberPattern = (function() {
+      var regexp = /^\(?(\d{3})\)?[ .-]?(\d{3})[ .-]?(\d{4})$/;
+      return {
+        test: function(value) {
+          if ($scope.requirePhone === false) {
+            return true;
+          } else {
+            return regexp.test(value);
+          }
+        }
+      };
+    })();
   }
 );
